2c2f798f7fc5cc1a5d33f3d8983b2d21eadc03ac
dedyk <dedykjp@gmail.com>
2014-09-14 21:42:19 +0200

    Release: 20140914 - Nowy slownik, dla googlebot'a dodatkowe blokowanie

--- src/main/java/pl/idedyk/japanese/dictionary/web/controller/RobotsController.java
+++ src/main/java/pl/idedyk/japanese/dictionary/web/controller/RobotsController.java
@@ -39,7 +39,17 @@ public class RobotsController {
 		StringBuffer robotsBody = new StringBuffer();
     	
 		robotsBody.append("User-agent: *\n");
-		robotsBody.append("Sitemap: " + baseServer + request.getContextPath() + "/sitemap.xml\n");
+		
+		robotsBody.append("Disallow: /wordDictionary/autocomplete\n");
+		
+		robotsBody.append("Disallow: /kanjiDictionary/saveCurrectTab\n");
+		robotsBody.append("Disallow: /kanjiDictionary/autocomplete\n");
+		robotsBody.append("Disallow: /kanjiDictionary/showAvailableRadicals\n");
+		robotsBody.append("Disallow: /kanjiDictionaryDetectSearch\n");
+		
+		robotsBody.append("Disallow: /suggestion/sendSuggestion\n\n");
+
+		robotsBody.append("User-agent: Googlebot\n");
 		
 		robotsBody.append("Disallow: /wordDictionary/autocomplete\n");
 		
@@ -49,6 +59,11 @@ public class RobotsController {
 		robotsBody.append("Disallow: /kanjiDictionaryDetectSearch\n");
 		
 		robotsBody.append("Disallow: /suggestion/sendSuggestion\n");
+		
+		robotsBody.append("Disallow: /wordDictionaryCatalog\n");
+		robotsBody.append("Disallow: /kanjiDictionaryCatalog\n\n");
+		
+		robotsBody.append("Sitemap: " + baseServer + request.getContextPath() + "/sitemap.xml\n");
 		
 		writer.append(robotsBody.toString());
     }
