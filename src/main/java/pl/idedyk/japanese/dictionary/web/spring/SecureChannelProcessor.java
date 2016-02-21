d619f079258c543b33e9b88542c42c2085aa7a7b
dedyk <dedykjp@gmail.com>
2016-02-21 13:37:36 +0100

    Panel administracyjny po SSL

---
+++ src/main/java/pl/idedyk/japanese/dictionary/web/spring/SecureChannelProcessor.java
@@ -0,0 +1,35 @@
+package pl.idedyk.japanese.dictionary.web.spring;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.util.Assert;
+
+public class SecureChannelProcessor extends org.springframework.security.web.access.channel.SecureChannelProcessor {
+
+	@Override
+    public void decide(FilterInvocation invocation, Collection<ConfigAttribute> config) throws IOException, ServletException {
+		
+        Assert.isTrue((invocation != null) && (config != null), "Nulls cannot be provided");
+
+        for (ConfigAttribute attribute : config) {
+        	
+            if (supports(attribute)) {
+            	
+            	HttpServletRequest httpRequest = invocation.getHttpRequest();
+                
+            	String xForwardedProto = httpRequest.getHeader("x-forwarded-proto");
+            	
+            	if (httpRequest.isSecure() == false && (xForwardedProto == null || xForwardedProto.equals("https") == false)) {
+            		
+                	getEntryPoint().commence(invocation.getRequest(), invocation.getResponse());
+                }
+            }
+        }
+    }
+}
