7009a843a67eab905108a241f9b50c7ecb7a91bf
dedyk <dedykjp@gmail.com>
2018-08-30 22:25:09 +0200

    Przygotowania do odchudzonej wersji

---
+++ src/main/java/pl/idedyk/japanese/dictionary/web/logger/model/WordDictionaryGetDictionaryEntriesNameSizeLoggerModel.java
@@ -0,0 +1,16 @@
+package pl.idedyk.japanese.dictionary.web.logger.model;
+
+public class WordDictionaryGetDictionaryEntriesNameSizeLoggerModel extends LoggerModelCommon {
+
+	private static final long serialVersionUID = 1L;
+		
+	public WordDictionaryGetDictionaryEntriesNameSizeLoggerModel(LoggerModelCommon loggerModelCommon) {
+		
+		super(loggerModelCommon);
+	}
+
+	@Override
+	public String toString() {
+		return "WordDictionaryGetDictionaryEntriesNameSizeLoggerModel []";
+	}
+}
