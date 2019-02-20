21950fdfae8d43ad62d38f1ceb9e1af35b9df7b2
dedyk <dedykjp@gmail.com>
2019-02-20 23:31:25 +0100

    Dodatkowa archiwizacja

--- sql/ddl-update15.sql
+++ sql/ddl-update15.sql
@@ -6,7 +6,7 @@ create table word_dictionary_unique_search_log (
     last_appearance_timestamp timestamp not null
 ) default character set = utf8 collate = utf8_polish_ci;
 
--- migracja danych:
+-- migracja danych (do uruchomienia kilka razy)
 
 insert into word_dictionary_unique_search_log(word, counter, first_appearance_timestamp, last_appearance_timestamp)
 select
@@ -17,6 +17,17 @@ select
 from
     word_dictionary_search_log wdsl, generic_log gl
 where
-    gl.id = wdsl.generic_log_id
+    gl.id = wdsl.generic_log_id and
+    substring(wdsl.find_word_request_word, 1, 65) not in (select word from word_dictionary_unique_search_log)
 group by
     substring(wdsl.find_word_request_word, 1, 65);
+
+-- sprawdzenie migracji, zapytanie nie moze zwrocic wynikow
+
+select
+     substring(wdsl.find_word_request_word, 1, 65)
+from
+    word_dictionary_search_log wdsl, generic_log gl
+where
+    gl.id = wdsl.generic_log_id and
+    0 = (select count(*) from word_dictionary_unique_search_log u where u.word = substring(wdsl.find_word_request_word, 1, 65))
