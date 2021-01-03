3f8f989d0d3e15821f50e7224b14fd6b8506e569
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2021-01-03 13:00:13 +0100

    Popraweczka NPE

--- src/main/java/pl/idedyk/japanese/dictionary/web/common/Utils.java
+++ src/main/java/pl/idedyk/japanese/dictionary/web/common/Utils.java
@@ -391,6 +391,10 @@ public class Utils {
 		
 		// http://detectmobilebrowsers.com/
 		
+		if (userAgent == null) {
+			return false;
+		}
+		
 		userAgent = userAgent.toLowerCase();
 		
 		return userAgent.matches("(?i).*((android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|wap|windows ce|xda|xiino).*") ||
