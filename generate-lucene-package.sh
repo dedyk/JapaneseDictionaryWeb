3398e3d4684f39154154aa180b950a92a293d3f5
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2022-01-02 16:32:53 +0100

    Dodanie brakujacych jar'ow do paczki generujaca baze danych lucene

--- generate-lucene-package.sh
+++ generate-lucene-package.sh
@@ -8,6 +8,7 @@ export JAPANESE_DICTIONARY_API_VERSION=1.0-SNAPSHOT
 export JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION=1.0-SNAPSHOT
 export LUCENE_VERSION=4.7.2
 export GSON_VERSION=2.8.5
+export LOG4J_VERSION=2.17.1
 
 cp $HOME/.m2/repository/pl/idedyk/japanese/JapaneseDictionaryAPI/$JAPANESE_DICTIONARY_API_VERSION/JapaneseDictionaryAPI-$JAPANESE_DICTIONARY_API_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/pl/idedyk/japanese/JapaneseDictionaryLuceneCommon/$JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION/JapaneseDictionaryLuceneCommon-$JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION.jar $BASE_DIR/lib
@@ -21,5 +22,7 @@ cp $HOME/.m2/repository/org/apache/lucene/lucene-sandbox/$LUCENE_VERSION/lucene-
 cp $HOME/.m2/repository/org/apache/lucene/lucene-suggest/$LUCENE_VERSION/lucene-suggest-$LUCENE_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/com/google/code/gson/gson/$GSON_VERSION/gson-$GSON_VERSION.jar $BASE_DIR/lib
 
+cp $HOME/.m2/repository/org/apache/logging/log4j/log4j-core/$LOG4J_VERSION/log4j-core-$LOG4J_VERSION.jar $BASE_DIR/lib
+cp $HOME/.m2/repository/org/apache/logging/log4j/log4j-api/$LOG4J_VERSION/log4j-api-$LOG4J_VERSION.jar $BASE_DIR/lib
 
 cp -r db $BASE_DIR/db
