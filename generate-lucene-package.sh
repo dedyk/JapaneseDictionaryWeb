beba697cea36e4dcddc40a9f4880016272a3fc94
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2022-05-01 17:01:24 +0200

    Dodanie jar google guava do generate-lucene-package.sh

--- generate-lucene-package.sh
+++ generate-lucene-package.sh
@@ -9,6 +9,7 @@ export JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION=1.0-SNAPSHOT
 export LUCENE_VERSION=4.7.2
 export GSON_VERSION=2.8.5
 export LOG4J_VERSION=2.17.1
+export GOOGLE_GUAVA=31.1-jre
 
 cp $HOME/.m2/repository/pl/idedyk/japanese/JapaneseDictionaryAPI/$JAPANESE_DICTIONARY_API_VERSION/JapaneseDictionaryAPI-$JAPANESE_DICTIONARY_API_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/pl/idedyk/japanese/JapaneseDictionaryLuceneCommon/$JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION/JapaneseDictionaryLuceneCommon-$JAPANESE_DICTIONARY_LUCENE_COMMON_VERSION.jar $BASE_DIR/lib
@@ -21,6 +22,7 @@ cp $HOME/.m2/repository/org/apache/lucene/lucene-queryparser/$LUCENE_VERSION/luc
 cp $HOME/.m2/repository/org/apache/lucene/lucene-sandbox/$LUCENE_VERSION/lucene-sandbox-$LUCENE_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/org/apache/lucene/lucene-suggest/$LUCENE_VERSION/lucene-suggest-$LUCENE_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/com/google/code/gson/gson/$GSON_VERSION/gson-$GSON_VERSION.jar $BASE_DIR/lib
+cp $HOME/.m2/repository/com/google/guava/guava/$GOOGLE_GUAVA/guava-$GOOGLE_GUAVA.jar $BASE_DIR/lib
 
 cp $HOME/.m2/repository/org/apache/logging/log4j/log4j-core/$LOG4J_VERSION/log4j-core-$LOG4J_VERSION.jar $BASE_DIR/lib
 cp $HOME/.m2/repository/org/apache/logging/log4j/log4j-api/$LOG4J_VERSION/log4j-api-$LOG4J_VERSION.jar $BASE_DIR/lib
