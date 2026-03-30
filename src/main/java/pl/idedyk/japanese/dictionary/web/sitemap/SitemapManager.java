f0664b727d1e09fd29d11344dd430a7445038829
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2026-03-30 17:02:50 +0200

    Name 2

--- src/main/java/pl/idedyk/japanese/dictionary/web/sitemap/SitemapManager.java
+++ src/main/java/pl/idedyk/japanese/dictionary/web/sitemap/SitemapManager.java
@@ -20,12 +20,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import pl.idedyk.japanese.dictionary.api.dto.DictionaryEntry;
-import pl.idedyk.japanese.dictionary.api.dto.DictionaryEntryType;
 import pl.idedyk.japanese.dictionary.web.common.LinkGenerator;
 import pl.idedyk.japanese.dictionary.web.dictionary.DictionaryManager;
 import pl.idedyk.japanese.dictionary.web.sitemap.exception.NotInitializedException;
 import pl.idedyk.japanese.dictionary2.jmdict.xsd.JMdict;
+import pl.idedyk.japanese.dictionary2.jmnedict.xsd.JMnedict;
 import pl.idedyk.japanese.dictionary2.kanjidic2.xsd.KanjiCharacterInfo;
 
 public class SitemapManager {
@@ -112,7 +111,9 @@ public class SitemapManager {
 	}
 	
 	private synchronized void generateSitemaps(boolean wait, String destDir, boolean deleteOnExit) throws Exception {
-				
+		
+		// FM_FIXME: sprawdzic, czy to dziala
+		
 		if (initialized == true) {
 			return;
 		}
@@ -143,7 +144,7 @@ public class SitemapManager {
 			// pobranie slowka
 			JMdict.Entry dictionaryEntry2 = dictionaryManager.getDictionaryEntry2ByCounter(currentDictionaryEntryIdx);
 			
-			createWordDictionaryLink(destDir, deleteOnExit, "wordDictionaryDetails", sitemapHelper, null, dictionaryEntry2);
+			createWordDictionaryLink(destDir, deleteOnExit, "wordDictionaryDetails", sitemapHelper, dictionaryEntry2, null);
 		}
 				
 		// katalog slow
@@ -164,9 +165,11 @@ public class SitemapManager {
 		for (int currentDictionaryEntryNameIdx = 1; currentDictionaryEntryNameIdx <= dictionaryEntriesNameSize; ++currentDictionaryEntryNameIdx) {
 			
 			// pobranie slowka
-			DictionaryEntry currentDictionaryEntry = dictionaryManager.getDictionaryEntryNameById(currentDictionaryEntryNameIdx);
+			JMnedict.Entry nameDictionaryEntry2 = dictionaryManager.getNameDictionaryEntry2ByCounter(currentDictionaryEntryNameIdx);
 			
-			createWordDictionaryLink(destDir, deleteOnExit, "wordNameDictionaryDetails", sitemapHelper, currentDictionaryEntry, null);
+			// FM_FIXME: sprawdzic, czy to dziala
+			
+			createWordDictionaryLink(destDir, deleteOnExit, "wordNameDictionaryDetails3", sitemapHelper, null, nameDictionaryEntry2);
 		}
 
 		// katalog slow(nazwa)
@@ -280,19 +283,21 @@ public class SitemapManager {
 		logger.info("Generowanie pliku sitemap zakonczone");
 	}
 	
-	private void createWordDictionaryLink(String destDir, boolean deleteOnExit, String groupName, SitemapHelper sitemapHelper, DictionaryEntry currentDictionaryEntry, JMdict.Entry dictionaryEntry2) throws Exception {
-				
+	private void createWordDictionaryLink(String destDir, boolean deleteOnExit, String groupName, SitemapHelper sitemapHelper, JMdict.Entry dictionaryEntry2, JMnedict.Entry nameDictionaryEntry2) throws Exception {
+		
+		// FM_FIXME: sprawdzic, czy to dziala
+		
 		// wygenerowanie linku standardowego
 		String link; 
 		boolean isName = false;
 		
-		if (dictionaryEntry2 != null && currentDictionaryEntry == null) {
+		if (dictionaryEntry2 != null && nameDictionaryEntry2 == null) {
 			link = LinkGenerator.generateDictionaryEntryDetailsLink("", dictionaryEntry2);
 			isName = false;
 			
-		} else if (dictionaryEntry2 == null && currentDictionaryEntry != null) {
-			link = LinkGenerator.generateDictionaryEntryDetailsLink("", currentDictionaryEntry, null);
-			isName = currentDictionaryEntry.isName();
+		} else if (dictionaryEntry2 == null && nameDictionaryEntry2 != null) {
+			link = LinkGenerator.generateNameDictionaryEntryDetailsLink("", nameDictionaryEntry2);
+			isName = true;
 			
 		} else {
 			throw new RuntimeException(); // to nigdy nie powinno zdarzyc sie
@@ -301,6 +306,7 @@ public class SitemapManager {
 		// dodanie linku			
 		sitemapHelper.createUrl(destDir, deleteOnExit, groupName, link, ChangeFreqEnum.weekly, BigDecimal.valueOf(isName == false ? 0.8 : 0.6));
 		
+		/*
 		if (currentDictionaryEntry != null) {
 			
 			// pobranie listy typow
@@ -337,6 +343,7 @@ public class SitemapManager {
 				}				
 			}
 		}
+		*/
 	}
 	
 	private void loadPregeneredSitemap(File pregeneredSitemapDir) {
