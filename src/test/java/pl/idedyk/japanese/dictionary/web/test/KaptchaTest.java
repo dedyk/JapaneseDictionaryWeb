159a2cffba65cf287e2f3c9ad3e3d3b00d5519e2
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2026-07-25 19:15:18 +0200

    Proste zabezpieczenie Captcha

---
+++ src/test/java/pl/idedyk/japanese/dictionary/web/test/KaptchaTest.java
@@ -0,0 +1,45 @@
+package pl.idedyk.japanese.dictionary.web.test;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Properties;
+
+import javax.imageio.ImageIO;
+
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+
+public class KaptchaTest {
+
+	public static void main(String[] args) throws Exception {
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        
+        Properties properties = new Properties();
+        
+        // properties.setProperty(Constants.KAPTCHA_IMAGE_HEIGHT, "640");
+        // properties.setProperty(Constants.KAPTCHA_IMAGE_WIDTH, "480");
+        
+        properties.setProperty("kaptcha.image.width", "250");
+        properties.setProperty("kaptcha.image.height", "50");
+        properties.setProperty("kaptcha.textproducer.char.length", "8");
+        properties.setProperty("kaptcha.textproducer.font.color", "black");
+        // properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.DefaultNoise");
+        
+        Config config = new Config(properties);
+        
+        kaptcha.setConfig(config);
+        
+        // String randomText = "Fryderyk";
+        
+        BufferedImage image = kaptcha.createImage(kaptcha.createText()); // createText(); // createImage(randomText);
+
+        ImageIO.setUseCache(false);
+        ImageIO.write(image, "png", new File("/tmp/a/test.png"));
+        
+        // String base64Image = Base64.getEncoder().encodeToString(baos.toByteArray());
+
+        //return new CaptchaResponse("data:image/png;base64," + base64Image);
+	}
+
+}
